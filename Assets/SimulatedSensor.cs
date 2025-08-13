5e0b91ed3ff342ba80dc4c3aa5f2419e838ca74f
wiQ1999 <wiktor.szczeszek99.other@gmail.com>
2025-08-13 20:57:10 +0200

    feat(SimulatedSensor): change sample to use `FakedCentralSequenceSunSensorSource` impl

--- Assets/SimulatedSensor.cs
+++ Assets/SimulatedSensor.cs
@@ -4,38 +4,25 @@ using UnityEngine;
 
 public class SimulatedSensor : MonoBehaviour
 {
-    //float timer = 0f;
-    ISunSensorRealtimeSource source = new FakedUsbSunSensorSource();
+    readonly ISunVectorRealtimeSource source = new FakedCentralSequenceSunSensorSource();
     Quaternion rotation = Quaternion.identity;
 
-    // Start is called before the first frame update
     private void Awake()
     {
         source.DataReceived += (data) =>
         {
             Debug.Log($"Data {data}");
-            rotation = Quaternion.Euler(
-                data.UnitVector.X,
-                data.UnitVector.Y,
-                data.UnitVector.Z);
+            rotation = Quaternion.LookRotation(data, Vector3.up);
         };
         source.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        /*
-        timer += Time.deltaTime;
-        
-        float x = Mathf.Sin(timer) * 30f;
-        float y = Mathf.Sin(timer * 0.5f) * 45f;
-        float z = Mathf.Sin(timer * 0.8f) * 60f;
-
-        transform.rotation = Quaternion.Euler(x, y, z);
-        */
-
-        transform.rotation = rotation;
+        if (source.IsActive)
+        {
+            transform.rotation = rotation;
+        }
     }
 
     private void OnDestroy()
