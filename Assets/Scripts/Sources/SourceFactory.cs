65043781fce4a6e28560f97b909382a0578b2367
wiQ1999 <wiktor.szczeszek99.other@gmail.com>
2025-08-18 21:51:14 +0200

    feat: add `SourceFactory`

---
+++ Assets/Scripts/Sources/SourceFactory.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Interfaces;
+using Assets.Scripts.Models.Config;
+using Assets.Scripts.Sources.UsbSunSensor;
+using System;
+
+namespace Assets.Scripts.Sources
+{
+    internal static class SourceFactory
+    {
+        internal static ISunVectorRealtimeSource CreateSunVectorRealtimeSource(AppSettings settings)
+        {
+            Enum.TryParse<AppMode>(settings.Mode, true, out var mode);
+            return mode switch
+            {
+                AppMode.TestCentralSequence => new FakedCentralSequenceSunSensorSource(),
+                _ => throw new NotImplementedException($"'{nameof(ISunVectorRealtimeSource)}' is not implemented for AppMode '{mode}'.")
+            };
+        }
+    }
+}
