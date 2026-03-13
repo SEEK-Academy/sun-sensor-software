5e98f84456be2299cb5c0b2f9814c86e0dec84b6
wiQ1999 <wiktor.szczeszek99.other@gmail.com>
2026-03-13 23:24:15 +0100

    feat: add interface for reading frame model source

---
+++ Assets/Scripts/Interfaces/ISunSensorFrameRealtimeSource.cs
@@ -0,0 +1,13 @@
+﻿using Sunsensor;
+using System;
+
+namespace Assets.Scripts.Interfaces
+{
+    public interface ISunSensorFrameRealtimeSource : IDisposable
+    {
+        event Action<Frame> FrameReceived;
+        bool IsActive { get; }
+        void Start();
+        void Stop();
+    }
+}
